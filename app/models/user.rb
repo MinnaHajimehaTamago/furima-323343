16378e1a9b11248aeec175a1b3af0e7167101e2a
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2020-12-12 22:17:18 +0900

    Rubocop適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise  :database_authenticatable, :registerable, 
+  devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   validates :nickname, presence: true
   validates :first_name, presence: true
@@ -12,5 +12,4 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
   validates :birth_date, presence: true
-  
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname              { Faker::Name.initials(number: 2) }
-    email                { Faker::Internet.free_email }
+    nickname { Faker::Name.initials(number: 2) }
+    email { Faker::Internet.free_email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name            { Faker::Name.first_name }
