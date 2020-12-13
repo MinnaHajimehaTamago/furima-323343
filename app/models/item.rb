e68fa1474594b455d25359057f72614131986db1
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2020-12-13 14:34:20 +0900

    ActiveHash用のモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  extend Active::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :days_to_ship
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :state
+
+  with_options numericality: { other_than:1 } do
+    validates :category_id
+    validates :days_to_ship
+    validates :delivery_fee
+    validates :prefecture
+    validates :state
+  end
 end
