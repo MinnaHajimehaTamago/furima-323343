1e88e3e0666f8f9a1d3a0a8c9fcdb718cba0b067
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2020-12-13 19:04:23 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,28 @@
 class Item < ApplicationRecord
-  extend Active::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :days_to_ship
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :state
 
-  with_options numericality: { other_than:1 } do
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image, presence: { message: 'を選択してください' }
+    validates :name
+    validates :text
+  end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は¥300~9,999,999で入力してください'}
+  validates :price, numericality: { only_integer: true, message: 'は半角数字で入力してください' }
+
+  with_options presence: true, numericality: { other_than: 1, message: 'を選択してください' } do
     validates :category_id
-    validates :days_to_ship
-    validates :delivery_fee
-    validates :prefecture
-    validates :state
+    validates :days_to_ship_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :state_id
   end
 end
