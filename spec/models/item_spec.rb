b24b14b133218ef8094be1e8709141486f29b35a
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2020-12-14 11:04:08 +0900

    テストコードに関する不要なファイルや記述を削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,34 +9,6 @@ RSpec.describe Item, type: :model do
       it '必須項目が入力されていれば登録できること' do
         expect(@item).to be_valid
       end
-      it 'category_idが1以外であれば登録できること' do
-        @item.category_id = '2'
-        expect(@item).to be_valid
-      end
-      it 'state_idが1以外であれば登録できること' do
-        @item.state_id = '2'
-        expect(@item).to be_valid
-      end
-      it 'delivery_fee_idが1以外であれば登録できること' do
-        @item.delivery_fee_id = '2'
-        expect(@item).to be_valid
-      end
-      it 'prefecture_idが1以外であれば登録できること' do
-        @item.prefecture_id = '2'
-        expect(@item).to be_valid
-      end
-      it 'days_to_ship_idが1以外であれば登録できること' do
-        @item.days_to_ship_id = '2'
-        expect(@item).to be_valid
-      end
-      it 'priceが300以上9999999以下であれば登録できること（最小値）' do
-        @item.price = 300
-        expect(@item).to be_valid
-      end
-      it 'priceが300以上9999999以下であれば登録できること（最大値）' do
-        @item.price = 9999999
-        expect(@item).to be_valid
-      end
     end
 
     context '商品が登録できない時' do
