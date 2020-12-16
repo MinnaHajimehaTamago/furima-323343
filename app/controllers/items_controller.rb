2a5c0f2cd89ae3de3b2c2d5f267a767ce6aa23f2
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2020-12-16 17:10:16 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :edit]
-  before_action :set_params, only: [:show, :edit, :update]
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: :edit
 
   def index
@@ -36,8 +36,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy if current_user.id == @item.user_id
     redirect_to root_path
   end
 
