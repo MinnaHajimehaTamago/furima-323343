2257d594c0ff8d7cebedfe3b41f73d437006dacb
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2020-12-29 16:23:05 +0900

    コメント表示情報の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      ActionCable.server.broadcast 'comment_channel', content: @comment
+      ActionCable.server.broadcast 'comment_channel', content: channel_params
     end
   end
 
@@ -10,5 +10,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def channel_params
+    params.require(:comment).permit(:text).merge(nickname: current_user.nickname)
   end
 end
